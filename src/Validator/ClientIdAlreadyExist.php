fbe387a67bde53b7f1dc5908fedca5fadf11380d
visa4 <antonino.visalli@gmail.com>
2015-11-14 19:22:07 +0100

    Fix validator

--- src/Validator/ClientIdAlreadyExist.php
+++ src/Validator/ClientIdAlreadyExist.php
@@ -2,6 +2,7 @@
 
 namespace Strapieno\Auth\Model\Validator;
 
+use Matryoshka\Model\ResultSet\ResultSetInterface;
 use Strapieno\Auth\Model\OauthClientModelAwareInterface;
 use Strapieno\Auth\Model\OauthClientModelAwareTrait;
 use Zend\Validator\AbstractValidator;
@@ -13,12 +14,29 @@ use Zend\Validator\Exception;
 class ClientIdAlreadyExist extends AbstractValidator implements OauthClientModelAwareInterface
 {
     use OauthClientModelAwareTrait;
+
+    const CLIENT_ID_ALREDY_EXYST = 'clientIdAlreadyExist';
+
+    /**
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::CLIENT_ID_ALREDY_EXYST => 'Cliend id must be unique'
+    ];
+
     /**
      * {@inheritdoc}
      */
     public function isValid($value)
     {
-        var_dump('test'); die();
+        // TODO add assetion manager
+        $criteria = (new OauthClientMongoCollectionCriteria())->setClientId($value);
+        /** @var ResultSetInterface $result */
+        $result  = $this->getOauthClientModelService()->find($criteria);
+        if ($result->count() > 0) {
+            $this->error(self::CLIENT_ID_ALREDY_EXYST);
+            return false;
+        }
+        return true;
     }
-
 }
