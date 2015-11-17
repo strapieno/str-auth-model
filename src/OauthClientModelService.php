c94997a7a586f048156d0930c09748a95116b7c7
visa4 <antonino.visalli@gmail.com>
2015-11-17 17:16:20 +0100

    Fix getAgetAuthenticationClient

--- src/OauthClientModelService.php
+++ src/OauthClientModelService.php
@@ -3,7 +3,8 @@
 namespace Strapieno\Auth\Model;
 
 use Matryoshka\Model\ObservableModel;
-use Matryoshka\Model\Wrapper\Mongo\Criteria\ActiveRecord\ActiveRecordCriteria;
+use Matryoshka\Model\ResultSet\ResultSetInterface;
+use Strapieno\Auth\Model\Criteria\Mongo\OauthClientMongoCollectionCriteria;
 use Strapieno\Auth\Model\Entity\OauthClientInterface;
 
 /**
@@ -17,10 +18,15 @@ class OauthClientModelService extends ObservableModel implements OauthClientMode
      */
     public function getAuthenticationClient($dataClient)
     {
-        if (is_array($dataClient) && isset($dataClient['_id'])) {
+        if (is_array($dataClient) && isset($dataClient['client_id'])) {
             // TODO criteria manager
-            $criteria = (new ActiveRecordCriteria())->setId((string) $dataClient['_id']);
-            return $this->find($criteria)->current();
+            $criteria = (new OauthClientMongoCollectionCriteria())->setClientId($dataClient['client_id']);
+            /** @var $resultSet ResultSetInterface */
+            $resultSet = $this->find($criteria);
+            if ($resultSet->count() == 1) {
+                return $resultSet->current();
+            }
+            throw new \RuntimeException(sprintf('Ambiguous client id %s', $dataClient['client_id']));
         }
         throw new \RuntimeException(sprintf('Invalid array data on %s', __METHOD__));
     }
