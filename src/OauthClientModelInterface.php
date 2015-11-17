94e0eb5f54b4d406ae32ef2adc72cc88f2db6a21
visa4 <antonino.visalli@gmail.com>
2015-11-17 16:28:33 +0100

    Add getAuthenticationClient

--- src/OauthClientModelInterface.php
+++ src/OauthClientModelInterface.php
@@ -6,5 +6,11 @@ namespace Strapieno\Auth\Model;
  * Interface OauthClientModelInterface
  */
 interface OauthClientModelInterface
-{}
+{
+    /**
+     * @param $dataClient
+     * @return OauthClientInterface|null
+     */
+    public function getAuthenticationClient($dataClient);
+}
 
--- src/OauthClientModelService.php
+++ src/OauthClientModelService.php
@@ -3,9 +3,25 @@
 namespace Strapieno\Auth\Model;
 
 use Matryoshka\Model\ObservableModel;
+use Matryoshka\Model\Wrapper\Mongo\Criteria\ActiveRecord\ActiveRecordCriteria;
+use Strapieno\Auth\Model\Entity\OauthClientInterface;
 
 /**
  * Class OauthClientModelService
  */
 class OauthClientModelService extends ObservableModel implements OauthClientModelInterface
-{}
+{
+    /**
+     * @param $dataClient
+     * @return OauthClientInterface|null
+     */
+    public function getAuthenticationClient($dataClient)
+    {
+        if (is_array($dataClient) && isset($dataClient['_id'])) {
+            // TODO criteria manager
+            $criteria = (new ActiveRecordCriteria())->setId((string) $dataClient['_id']);
+            return $this->find($criteria)->current();
+        }
+        throw new \RuntimeException(sprintf('Invalid array data on %s', __METHOD__));
+    }
+}
