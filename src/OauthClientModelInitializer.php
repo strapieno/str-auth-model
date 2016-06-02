0d1b2180d28116b2d4bc1851c0d0bed986da5c9f
visa4 <antonino.visalli@gmail.com>
2016-06-02 13:34:32 +0200

    Fix initializer

--- src/OauthClientModelInitializer.php
+++ src/OauthClientModelInitializer.php
@@ -1,11 +1,12 @@
 <?php
 namespace Strapieno\Auth\Model;
 
+use Strapieno\Utils\Initializer\AbstractModelServiceInitializer;
 
 /**
  * Class OauthClientModelInitializer
  */
-class OauthClientModelInitializer
+class OauthClientModelInitializer extends AbstractModelServiceInitializer
 {
     const SERVICE_NAME = OauthClientModelService::class;
     const INSTANCE_CLASS = OauthClientModelAwareInterface::class;
