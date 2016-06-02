8dfa39919a3a76242c3390eef42cec66c119ecdd
visa4 <antonino.visalli@gmail.com>
2016-06-02 13:22:18 +0200

    Test

--- tests/OauthClientModelAwareTraitTest.php
+++ tests/OauthClientModelAwareTraitTest.php
@@ -4,12 +4,9 @@ namespace Strapieno\Auth\ModelTest;
 use Matryoshka\Model\Object\ActiveRecord\AbstractActiveRecord;
 use Strapieno\Auth\Model\Entity\OauthUserClientEntity;
 use Strapieno\Auth\Model\OauthClientModelAwareTrait;
-use Strapieno\Auth\Model\OauthClientModelInitializer;
-use Strapieno\Auth\Model\OauthClientModelInitializer2;
 use Strapieno\Utils\Model\Entity\DateHistoryAwareTrait;
 use Strapieno\Utils\Model\Entity\RoleAwareTrait;
 use Strapieno\User\Model\Entity\UserIdAwareTrait;
-use Zend\ServiceManager\Di\DiServiceInitializer;
 
 /**
  * Class OauthClientEntity
